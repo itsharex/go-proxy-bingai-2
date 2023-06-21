5ed6c4a2d44142d3c6f10d3c5d697fb70cecc173
chenji <262338356@qq.com>
2023-06-22 03:55:25 +0800

    Update index.go

--- api/index.go
+++ api/index.go
@@ -1,8 +1,8 @@
 package api
 
 import (
-	"adams549659584/go-proxy-bingai/api/helper"
-	"adams549659584/go-proxy-bingai/common"
+	"javadeath/go-proxy-bingai/api/helper"
+	"javadeath/go-proxy-bingai/common"
 	"net/http"
 	"strings"
 )
